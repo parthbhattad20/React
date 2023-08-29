721605f58ed60637c2ab1a4c072d261bc63ce137
Parth Bhattad <bhattadparth20@gmail.com>
2023-08-29 18:44:10 +0530

    second

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,13 +11,13 @@ const Footer = () => {
         </a>
         <p className="mt-4 text-gray-400">Follow us on social media:</p>
         <div className="flex justify-center mt-2 space-x-4">
-          <a href="#" className="text-blue-400 hover:text-blue-300">
+          <a href="/" className="text-blue-400 hover:text-blue-300">
             Facebook
           </a>
-          <a href="#" className="text-blue-400 hover:text-blue-300">
+          <a href="/" className="text-blue-400 hover:text-blue-300">
             Twitter
           </a>
-          <a href="#" className="text-blue-400 hover:text-blue-300">
+          <a href="/" className="text-blue-400 hover:text-blue-300">
             Instagram
           </a>
         </div>
